f99a24aa143744b5690c1fd496c0829b9143c020
levkris <105733478+levkris@users.noreply.github.com>
2025-08-28 16:47:20 +0200

    fixed .pfp

--- webpage/assets/js/globalFunctions.js
+++ webpage/assets/js/globalFunctions.js
@@ -1210,11 +1210,12 @@ if (fileInput) {
 
 function getAssetType(fileName) {
   const parts = fileName.toLowerCase().split('.');
-  const ext = parts[parts.length - 1];
 
-  if (parts.length >= 3 && parts[parts.length - 2] === 'pfp' && ['png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp'].includes(ext)) {
+  if (parts.length >= 3 && parts[parts.length - 1] === 'pfp' && ['png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp'].includes(parts[parts.length - 2])) {
     return 'profile_picture';
   }
+  
+  const ext = parts[parts.length - 1];
 
   if (['png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp'].includes(ext)) return 'image';
   if (['mp4', 'webm', 'ogg'].includes(ext)) return 'video';
