7e3e8a0831f68c64d36d8f07784c9fc0e633ed7e
levkris <105733478+levkris@users.noreply.github.com>
2025-08-28 15:51:38 +0200

    Better way for staff detection

--- backend/server/helpers/user_helpers.py
+++ backend/server/helpers/user_helpers.py
@@ -93,7 +93,9 @@ async def get_user_info_from_id(cur, user_id):
         "profile_picture": rows[0]["profile_picture"],
         "premium": await get_user_premium_status(cur, user_id),
         "bot": False,
-        "tags": []
+        "tags": [],
+        "staff": await is_user_staff(cur, user_id),
+        "developer": await is_user_developer(cur, user_id)
     }
 
     for row in rows:
@@ -177,3 +179,13 @@ async def get_user_info(sid, data):
                 return
 
             await sio_instance.sio.emit('user_info', {'error': 'User/bot not found', 'req_id': req_id}, to=sid)
+            
+async def is_user_staff(cur, user_id):
+    await cur.execute("SELECT is_staff FROM users WHERE id = %s", (user_id,))
+    row = await cur.fetchone()
+    return row[0] == 1
+
+async def is_user_developer(cur, user_id):
+    await cur.execute("SELECT is_developer AND is_staff FROM users WHERE id = %s", (user_id,))
+    row = await cur.fetchone()
+    return row[0] == 1 and row[1] == 1
