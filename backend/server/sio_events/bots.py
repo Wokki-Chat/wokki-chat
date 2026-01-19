b8e7e820f73240d9f7bc9c54abbb5f175c7f7c43
Bjarnos <115639@leerling.stedelijkgymnijmegen.nl>
2026-01-19 14:55:30 +0100

    Added debug log

--- backend/server/sio_events/bots.py
+++ backend/server/sio_events/bots.py
@@ -1,9 +1,11 @@
 from server.sio_instance import sio
 from server.helpers.bot_message_helpers import send_bot_message, edit_bot_message, delete_bot_message, embed_button
 from server.helpers.bot_helpers import initialize_commands
+from server.helpers.logs import addMessageToLogs # temporary
     
 @sio.on('send_bot_message')
 async def handle_send_bot_message(sid, data):
+    await addMessageToLogs(f"debug log for bjarnos", "INFO")
     await send_bot_message(sid, data)
 
 @sio.on('edit_bot_message')
