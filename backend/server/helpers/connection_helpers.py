5358207225c634f003c443403433d7dc9d1a4a3d
levkris <105733478+levkris@users.noreply.github.com>
2026-01-19 14:28:18 +0100

    fixes for offline and online

--- backend/server/helpers/connection_helpers.py
+++ backend/server/helpers/connection_helpers.py
@@ -210,14 +210,14 @@ async def handle_delayed_disconnect(user_id, disconnect_token):
 
     async with config.pool.acquire() as conn:
         async with conn.cursor() as cur:
-            if await redis_client.get(disconnect_key) != disconnect_token:
+            if await redis_client.get(disconnect_key) != disconnect_token and await get_sids_for_user(user_id) != []:
                 await cur.execute(
                     "UPDATE users SET status = 'online' WHERE id = %s AND status_manually_set = FALSE",
                     (user_id,)
                 )
                 await conn.commit()
                 await broadcast_user_update(user_id)
-                await addMessageToLogs(f"User {user_id} reconnected before timeout, skipping offline, user set to online", "INFO")
+                await addMessageToLogs(f"User {user_id} reconnected before timeout, skipping offline, user set to online. Users sids: {await get_sids_for_user(user_id)}", "INFO")
                 return
             
             await cur.execute(
