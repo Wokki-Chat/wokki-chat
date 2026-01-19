caafe1856a0590c2d7df608c26c1c72955356656
Bjarnos <115639@leerling.stedelijkgymnijmegen.nl>
2026-01-19 16:40:04 +0100

    Global sio error handling now has correct file & line

--- backend/server/sio_config.py
+++ backend/server/sio_config.py
@@ -1,6 +1,6 @@
-import traceback
+import traceback, os
 from server.helpers.connection_helpers import handle_connect, handle_disconnect
-from server.helpers.logs import addMessageToLogs
+from server.helpers.logs import betterstack_logger, BETTERSTACK_TOKEN, addMessageToLogs
 from server.config import server_name
 from server.sio_instance import sio
 
@@ -19,5 +19,24 @@ async def disconnect(sid):
 @sio.on_error_default
 async def catch_all_sio_errors(sid, exc, event=None, data=None):
     tb_str = "".join(traceback.format_exception(type(exc), exc, exc.__traceback__))
-    msg = f"Error in event '{event}' with data {data}:\n{tb_str}"
-    addMessageToLogs(msg, 'ERROR')
+    
+    tb = exc.__traceback__
+    while tb.tb_next:
+        tb = tb.tb_next
+    caller_file = os.path.basename(tb.tb_frame.f_code.co_filename)
+    caller_line = tb.tb_lineno
+
+    msg = f"Error in event '{event}' with data '{data}':\n{tb_str}"
+
+    if BETTERSTACK_TOKEN:
+        # send directly with modified file and line
+        log_data = {
+            'worker': server_name,
+            'file': caller_file,
+            'line': caller_line,
+            'log_type': "ERROR"
+        }
+        betterstack_logger.error(msg, extra=log_data)
+    else:
+        # send through wrapper
+        addMessageToLogs(msg, 'ERROR')
