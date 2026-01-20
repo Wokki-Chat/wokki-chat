d2eff9b506ebf371ae2d0f98b0f195606d2cee69
Bjarnos <115639@leerling.stedelijkgymnijmegen.nl>
2026-01-20 16:21:18 +0100

    Readded old push

--- backend/server/helpers/message_helpers.py
+++ backend/server/helpers/message_helpers.py
@@ -112,10 +112,10 @@ async def send_message(sid, metadata, data):
                 await cur.execute(
                     '''
                     INSERT INTO bot_messages 
-                    (id, message, bot_id, created_at, updated_at, edited, server_id, channel_id, command, command_user_id, embed)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    (id, message, bot_id, created_at, updated_at, edited, server_id, channel_id, command, command_user_id, embed, assets, parent_message_id)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                     ''',
-                    (message_id, message, account_id, timestamp, None, False, server_id, channel_id, command, user_id, embed_str)
+                    (message_id, message, account_id, timestamp, None, False, server_id, channel_id, command, user_id, embed_str, assets_json, parent_message_id)
                 )
                 await addMessageToLogs(f"Inserted bot message for bot id: {account_id}", "INFO")
             else:
