70f78b8a299bbc18a83b9e63fedb4d31a5d6f6ee
levkris <105733478+levkris@users.noreply.github.com>
2026-01-21 13:09:55 +0100

    more css fixes

--- webpage/developer/bots.php
+++ webpage/developer/bots.php
@@ -49,7 +49,6 @@ $botsStmt->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wokki Chat Developer Portal</title>
     <link rel="stylesheet" href="../assets/styles/developer/main.css">
-    <link rel="stylesheet" href="../assets/styles/developer/bots.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="icon" type="image/x-icon" href="../favicon.ico">
 </head>
