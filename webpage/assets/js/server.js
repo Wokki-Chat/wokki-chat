9a17f3e39c1b8c3df322ebab32bee44098048680
levkris <105733478+levkris@users.noreply.github.com>
2026-01-24 12:39:12 +0100

    js fix

--- webpage/assets/js/server.js
+++ webpage/assets/js/server.js
@@ -499,7 +499,7 @@ function initServer() {
 				: ''
 			}
 			${
-				command_info !== null && command_info.user_id !== null
+				command_info !== null && command_info?.user_id !== null
 				? `<div class="message-command">
 					<img class="identification" src="/assets/images/identifier.svg">
 					<p class="username-command">@${command_info.username ?? ''}</p>
