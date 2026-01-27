9eee2031ba1c95b448689b6f576e8a7fc397646e
levkris <105733478+levkris@users.noreply.github.com>
2026-01-27 15:35:07 +0100

    swup testing

--- webpage/server.php
+++ webpage/server.php
@@ -718,11 +718,15 @@ if (!$server_id || !isset($user_servers[$server_id])) {
         <wchat-data id="profile-picture-url" value="<?php echo htmlspecialchars($profile_picture); ?>"></wchat-data>
         <script src="/assets/js/emojis.js"></script>
     </main>
+    <div id="settings">
+
+    </div>
     <script src="/assets/js/socket.js" data-swup-ignore-script></script>
     <script type="module" data-swup-ignore-script>
         import Swup from "https://unpkg.com/swup@4?module";
         import SwupPreloadPlugin from "https://unpkg.com/@swup/preload-plugin@3?module";
         import SwupScriptsPlugin from "https://unpkg.com/@swup/scripts-plugin@2?module";
+        import FragmentPlugin from 'https://www.unpkg.com/@swup/fragment-plugin@1?module';
 
         window.swup = new Swup({
             containers: ["#app"],
@@ -732,6 +736,15 @@ if (!$server_id || !isset($user_servers[$server_id])) {
                 new SwupScriptsPlugin({
                     body: true,
                     head: false,
+                }),
+                new SwupFragmentPlugin({
+                    rules: [
+                        {
+                            from: '/server/:server_id/channel/:channel_id',
+                            to: '/settings',
+                            containers: ['#settings']
+                        }
+                    ]
                 })
             ]
         });
