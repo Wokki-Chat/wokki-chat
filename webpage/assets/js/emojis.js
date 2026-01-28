4b5aa33f30a96f6d251b89d846b91f741f0d3103
levkris <105733478+levkris@users.noreply.github.com>
2026-01-28 13:38:42 +0100

    emoji fixes

--- webpage/assets/js/emojis.js
+++ webpage/assets/js/emojis.js
@@ -35,6 +35,12 @@ window.emojis = {
 		if (!this.all) return;
 		const promises = this.all.map(async e => {
 			if (!e.emoji) return;
+
+			if (e.emojiSvg !== true) {
+				this.iconCache[e.emoji] = e.emoji;
+				return;
+			}
+
 			const hex = Array.from(e.emoji).map(c => c.codePointAt(0).toString(16)).join('-');
 			const url = `/assets/icons/emojis/${hex}.svg`;
 
