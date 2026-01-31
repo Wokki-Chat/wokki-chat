4ae6c683250214fa93185360f6087f20466eb104
levkris <105733478+levkris@users.noreply.github.com>
2026-01-31 14:20:11 +0100

    fixed textarea

--- webpage/assets/js/modules/servers/settings.js
+++ webpage/assets/js/modules/servers/settings.js
@@ -65,6 +65,110 @@ export default class SettingsManager {
 
                 const contentContainer = popupEl.querySelector("#settings-page-content");
                 if (contentContainer) contentContainer.innerHTML = pageContent;
+                const textarea = document.getElementById("message-input");
+                const preview = document.getElementById("message-input-bg");
+
+                const minHeight = 18;
+
+                if (textarea) {
+                    const maxHeight = 250;
+                    const warningThreshold = 200;
+                    const maxChars = 200;
+
+                    const messageInputWrapper = document.querySelector('.message-input-wrapper');
+
+                    const maxMessageLengthEl = document.querySelector('.max-message-length');
+                    const maxCharactersLeftEl = document.querySelector('.max-characters-left');
+
+                    document.querySelector(".input-container-2").addEventListener("click", () => textarea.focus());
+                    
+                    const inputContainer = document.querySelector(".input-container-2");
+
+                    const updateHeight = () => {
+                        let newHeight = Math.min(textarea.scrollHeight, maxHeight);
+                        messageInputWrapper.style.height = newHeight + 'px';
+                        inputContainer.style.minHeight = newHeight + 'px';
+                    };
+
+                    const updateCharsLeft = () => {
+                        const charsLeft = maxChars - textarea.innerText.length;
+                        if (charsLeft <= warningThreshold) {
+                            maxMessageLengthEl.style.display = 'flex';
+                            maxCharactersLeftEl.textContent = charsLeft;
+                            maxCharactersLeftEl.classList.toggle('debt', charsLeft < 0);
+                        } else {
+                            maxMessageLengthEl.style.display = 'none';
+                            maxCharactersLeftEl.classList.remove('debt');
+                        }
+                    };
+
+                    textarea.addEventListener('input', (e) => {
+                        if (e.target !== textarea) return;
+
+                        if (textarea.textContent.trim() === '' && textarea.innerHTML !== '') {
+                            textarea.innerHTML = '';
+                        }
+
+                        updateHeight();
+                        updateCharsLeft();
+
+                        preview.innerHTML = sanitize(textarea.innerText);
+                    });
+
+                    textarea.addEventListener('input', (e) => {
+                        if (e.target !== textarea) return;
+
+                        if (textarea.textContent.trim() === '' && textarea.innerHTML !== '') {
+                            textarea.innerHTML = '';;
+                        }
+
+                        updateHeight();
+                        updateCharsLeft();
+
+                        preview.innerHTML = sanitize(textarea.innerText);
+                    });
+
+                    textarea.addEventListener('paste', (e) => {
+                        e.preventDefault();
+
+                        const text = e.clipboardData.getData('text/plain');
+
+                        const selection = window.getSelection();
+                        if (!selection.rangeCount) return;
+                        selection.deleteFromDocument();
+                        selection.getRangeAt(0).insertNode(document.createTextNode(text));
+
+                        selection.collapseToEnd();
+
+                        textarea.dispatchEvent(new Event('input'));
+                    });
+
+                    textarea.addEventListener('keydown', (e) => {
+                        if (e.key === 'Enter') {
+                            e.preventDefault();
+
+                            const selection = window.getSelection();
+                            if (!selection.rangeCount) return;
+
+                            const range = selection.getRangeAt(0);
+                            range.deleteContents();
+
+                            const textNode = document.createTextNode('\n\n');
+                            range.insertNode(textNode);
+
+                            range.setStartAfter(textNode);
+                            range.setEndAfter(textNode);
+                            selection.removeAllRanges();
+                            selection.addRange(range);
+
+                            textarea.dispatchEvent(new Event('input'));
+                        }
+                    });
+
+                    updateHeight();
+                    updateCharsLeft();
+                    preview.innerHTML = sanitize(textarea.innerText);
+                }
             });
         });
 
