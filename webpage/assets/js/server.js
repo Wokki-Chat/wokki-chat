09fc8b65008396910f21d7a070d8e8c752d776e5
levkris <105733478+levkris@users.noreply.github.com>
2026-02-06 20:47:57 +0100

    more logging

--- webpage/assets/js/server.js
+++ webpage/assets/js/server.js
@@ -788,12 +788,11 @@ function initServer() {
 	}
 
 	function deleteMsg(id) {
+		console.log('deleteMsg', id);
 		const indexInCache = messageCache.delete(id);
-		console.log(indexInCache);
 		if (indexInCache === -1) return;
 
 		const nextMsg = messageCache.cache[indexInCache];
-		console.log(nextMsg);
 		if (!nextMsg) return;
 
 		const nextEl = nextMsg.el;
