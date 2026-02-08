fa8f4061c64d38eaeaf7e8dacaee0ef091f182e9
levkris <105733478+levkris@users.noreply.github.com>
2026-02-08 14:17:55 +0100

    js fixes

--- webpage/assets/js/modules/users/popups.js
+++ webpage/assets/js/modules/users/popups.js
@@ -227,10 +227,10 @@ export class UserPopupManager {
             popup.dataset.customStyle = 'true';
         }
         let userBio = await this.sanitizer.sanitizeMrk(user.bio);
-        userBio = await emojis.replaceText(userBio);
         if (userBio.length > 55) {
             userBio = userBio.slice(0, 55) + '<span class="cutoff">...</span>';
         }
+        userBio = await emojis.replaceText(userBio);
         popup.innerHTML = `
             ${user.profile_banner ? `<img draggable="false" class="user-info-profile-popup-banner" src="${user.profile_banner}">` : ''}
             <div class="user-info-profile-popup-profile-picture-username-status">
