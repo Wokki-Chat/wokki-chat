03e3a213fdfd710693f687965cde0c2daebcfff5
levkris <105733478+levkris@users.noreply.github.com>
2026-02-08 16:22:27 +0100

    fixed the built-in commands

--- webpage/assets/js/globalFunctions.js
+++ webpage/assets/js/globalFunctions.js
@@ -191,8 +191,8 @@ function placeCaretAtEnd(el) {
 }
 
 async function showUpdateInfo(sanitizer) {
-	const versionData = await fetch('version-info.json').then(res => res.json());
-	const updateMarkdown = await fetch(versionData["update-info"]).then(res => res.text());
+	const versionData = await fetch('/version-info.json').then(res => res.json());
+	const updateMarkdown = await fetch('/' + versionData["update-info"]).then(res => res.text());
 	const updateHTML = sanitizer.sanitizeMsg(updateMarkdown);
 
 	jspt.makePopup({
