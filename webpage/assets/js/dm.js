b408e20922e9981b3e0ee2806e8656dc26726291
levkris <105733478+levkris@users.noreply.github.com>
2026-02-11 16:19:38 +0100

    js fixes for Members list in contact groups

--- webpage/assets/js/dm.js
+++ webpage/assets/js/dm.js
@@ -76,6 +76,55 @@ function initDm() {
 
     loadMessages();
 
+	if (contact_type == "individual") {
+		staticProfileManager.openStaticProfile(contact_user_id, usersContainer);
+	} else {
+		socket.emit("get_contact_users", { access_token, contact_id });
+
+		socket.on("user_widget_updated", (data) => {
+			if (!groupUsersContainer) return;
+			const userIndex = usersList.findIndex(u => u.id === data.id);
+			if (userIndex !== -1) {
+				usersList[userIndex] = { ...usersList[userIndex], ...data };
+				userRenderer.render(usersList[userIndex], groupUsersContainer);
+			}
+
+			const popup = document.querySelector(".user-info-profile-popup");
+			if (popup && popup.dataset.userId === data.id) {
+				const spotify = data.Spotify;
+				if (spotify && spotify.item) userRenderer.updateSpotifyPopup(popup, spotify);
+				else {
+					const spotifyContainer = popup.querySelector(".dm-info-container .spotify-info")?.parentElement;
+					if (spotifyContainer) spotifyContainer.remove();
+					if (popup.spotifyInterval) clearInterval(popup.spotifyInterval);
+				}
+			}
+		});
+
+		socket.on("contact_users", async (users) => {
+			if (!groupUsersContainer) return;
+			groupUsersContainer.innerHTML = "";
+
+			usersList = users;
+
+			sanitizer.init(users);
+			mentions.init(users);
+			messageHandler.initU(users);
+
+			await Promise.all(users.map(user => {
+				userRenderer.render(user, groupUsersContainer);
+			}));
+		});
+
+		socket.on("user_updated", async (user) => {
+			if (!groupUsersContainer) return;
+			const userIndex = usersList.findIndex(u => String(u.id) === user.id);
+			if (userIndex === -1) return;
+			
+			userRenderer.render(usersList[userIndex], groupUsersContainer);
+		});
+	}
+
     messageContainer.addEventListener("scroll", () => {
         if (messageContainer.scrollTop === 0) {
             offset += limit;
@@ -300,55 +349,6 @@ function initDm() {
 			messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
 		}
 	});
-
-	if (contact_type == "individual") {
-		staticProfileManager.openStaticProfile(contact_user_id, usersContainer);
-	} else {
-		socket.emit("get_contact_users", { access_token, contact_id });
-
-		socket.on("user_widget_updated", (data) => {
-			if (!groupUsersContainer) return;
-			const userIndex = usersList.findIndex(u => u.id === data.id);
-			if (userIndex !== -1) {
-				usersList[userIndex] = { ...usersList[userIndex], ...data };
-				userRenderer.render(usersList[userIndex], groupUsersContainer);
-			}
-
-			const popup = document.querySelector(".user-info-profile-popup");
-			if (popup && popup.dataset.userId === data.id) {
-				const spotify = data.Spotify;
-				if (spotify && spotify.item) userRenderer.updateSpotifyPopup(popup, spotify);
-				else {
-					const spotifyContainer = popup.querySelector(".dm-info-container .spotify-info")?.parentElement;
-					if (spotifyContainer) spotifyContainer.remove();
-					if (popup.spotifyInterval) clearInterval(popup.spotifyInterval);
-				}
-			}
-		});
-
-		socket.on("contact_users", async (users) => {
-			if (!groupUsersContainer) return;
-			groupUsersContainer.innerHTML = "";
-
-			usersList = users;
-
-			sanitizer.init(users);
-			mentions.init(users);
-			messageHandler.initU(users);
-
-			await Promise.all(users.map(user => {
-				userRenderer.render(user, groupUsersContainer);
-			}));
-		});
-
-		socket.on("user_updated", async (user) => {
-			if (!groupUsersContainer) return;
-			const userIndex = usersList.findIndex(u => String(u.id) === user.id);
-			if (userIndex === -1) return;
-			
-			userRenderer.render(usersList[userIndex], groupUsersContainer);
-		});
-	}
 }
 
 if (typeof window.swup !== "undefined") {
