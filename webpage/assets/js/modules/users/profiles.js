776edaa864fbfb094242379909ce2db3b2a55e9d
levkris <105733478+levkris@users.noreply.github.com>
2026-02-11 16:10:48 +0100

    Fixes for userrenderer

--- webpage/assets/js/modules/users/profiles.js
+++ webpage/assets/js/modules/users/profiles.js
@@ -761,7 +761,7 @@ export class UserRenderer extends UserPopupManager {
 				</div>
 			</div>
 			<div class="self-info-status-username">
-				<div class="self-info-profile-username-container"><p class="self-info-username">${user.display_name ? sanitizer.sanitize(user.display_name) : sanitizer.sanitize(user.username)}</p>
+				<div class="self-info-profile-username-container"><p class="self-info-username">${user.display_name ? this.sanitizer.sanitize(user.display_name) : this.sanitizer.sanitize(user.username)}</p>
 					${user.bot ? '<div class="bot-tag"><span class="material-symbols-rounded">check</span>BOT</div>' : ''}
 					</div>
 				<p class="self-info-status">${user.status.charAt(0).toUpperCase() + user.status.slice(1)}</p>
